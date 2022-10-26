00f35f024e3f21ebb434a00ebb3b1a608b47f715
Ludwig Lebrun <lle@eonix.be>
2022-10-26 18:22:16 +0200

    Projet "Loop"

--- Sources/Loop/Program.cs
+++ Sources/Loop/Program.cs
@@ -12,37 +12,9 @@ PrintOtherLinqFunctions();
 
 PrintForI();
 
-void PrintSkipTake()
-{
-    var integers = new List<int>()
-    {
-        1,
-        45,
-        200,
-        200,
-        150,
-        12
-    };
+PrintForEach();
 
-// LINQ : 'Skip' signifie que je passe les deux premiers éléments de ma liste. Combiné avec Take(1) signifie que j'afficherai le troisième élément.
-    foreach (var integer in integers.Skip(2).Take(1))
-        Console.WriteLine(integer); // affichera 200
-
-// Méthode 'vanilla'
-    int i = 0;
-    foreach (int integer in integers)
-    {
-        i++;
-
-        if (i <= 2)
-            continue; // continue signifie que je passe à l'itération suivante
-
-        Console.WriteLine(integer);
-
-        if (i == 3)
-            break; // break signfie que je stoppe l'exécution de la boucle.
-    }
-}
+PrintWhile();
 
 void PrintTake()
 {
@@ -56,11 +28,11 @@ void PrintTake()
         12
     };
 
-// LINQ. 'Take' signifie que je ne prends que les 3 premiers éléments.
+    // LINQ. 'Take' signifie que je ne prends que les 3 premiers éléments.
     foreach (var integer in integers.Take(3))
         Console.WriteLine(integer); // affichera 1, 45, 200.
 
-// équivalent en code "vanilla"
+    // équivalent en code "vanilla"
     int i = 0;
     foreach (int integer in integers)
     {
@@ -68,6 +40,38 @@ void PrintTake()
         i++; // équivalent de i = i + 1;
         if (i == 3)
             break;
+    }
+}
+
+void PrintSkipTake()
+{
+    var integers = new List<int>()
+    {
+        1,
+        45,
+        200,
+        200,
+        150,
+        12
+    };
+
+    // LINQ : 'Skip' signifie que je passe les deux premiers éléments de ma liste. Combiné avec Take(1) signifie que j'afficherai le troisième élément.
+    foreach (var integer in integers.Skip(2).Take(1))
+        Console.WriteLine(integer); // affichera 200
+
+    // Méthode 'vanilla'
+    int i = 0;
+    foreach (int integer in integers)
+    {
+        i++;
+
+        if (i <= 2)
+            continue; // continue signifie que je passe à l'itération suivante
+
+        Console.WriteLine(integer);
+
+        if (i == 3)
+            break; // break signfie que je stoppe l'exécution de la boucle.
     }
 }
 
@@ -85,12 +89,13 @@ void PrintOtherLinqFunctions()
 
     var test = integers.First(x => x == 200);
     Console.WriteLine(test); // affichera 200
-// // .First() me donnera le premier élément qui satisfait à l'égalité "x == 200".
-// // x => x est une 'lambda expression'. Cela nous permet de facilement itérer entre les éléments de notre liste.
-// // Avec .First() si l'élément n'est pas trouvé, une exception sera levée. Si j'ai plusieurs élément, ce sera le premier rencontré.
+    // // .First() me donnera le premier élément qui satisfait à l'égalité "x == 200".
+    // // x => x est une 'lambda expression'. Cela nous permet de facilement itérer entre les éléments de notre liste.
+    // // Avec .First() si l'élément n'est pas trouvé, une exception sera levée. Si j'ai plusieurs élément, ce sera le premier rencontré.
 
-// var testException = integers.First(x => x == 201);
-// // 201 n'étant pas dans la liste, ce statement provoquera une exception.
+    // var testException = integers.First(x => x == 201);
+    // // 201 n'étant pas dans la liste, ce statement provoquera une exception.
+    var testNoException = integers.FirstOrDefault(x => x == 201); // affichera 0, car retourne la valeur 'par défaut' d'un entier, à savoir 0 
 
     var students = new List<Student>()
     {
@@ -112,7 +117,7 @@ void PrintOtherLinqFunctions()
     };
 
 // // .Where() me permet de récupérer une liste d'élement remplissant une/des condition(s) particulières.
-    var test2 = students.Where(x => x.LastName == "Leblanc" && x.FirstName == "Jacques").ToArray();
+    var test2 = students.Where(student => student.LastName == "Leblanc" && student.FirstName == "Jacques").ToArray();
     Console.WriteLine(test2.Length); // affichera '2'
 // // Dans le cas présent, parmit la liste "students", nous voulons récupérer tous les élèves qui ont pour nom 'Leblanc' et comme prénom 'Jacques'
     var test3 = students.Exists(x => x.LastName == "Leblanc" && x.FirstName == "Jacques");
@@ -121,6 +126,20 @@ void PrintOtherLinqFunctions()
     var test4 = students.All(x => x.LastName == "Leblanc");
     Console.WriteLine(test4); // affichera 'true'
 // // Avec "All" le résultat sera un booléen qui indique si tous les éléments de cette liste remplissent la condition LastName == 'Leblanc'.
+    var test5 = students.Select(x => x.FirstName).ToList();
+    // Console.WriteLine(test5.Select(x => $"{x}, "));
+    Console.WriteLine(string.Join(", ", test5)); // affichera 'Jacques, Jacques, James'
+    var test5String = "";
+    foreach (var myString in test5)
+    {
+        // Jacques, 
+        // Jacques, Jacques, 
+        // Jacques, Jacques, James, 
+        test5String += myString + ", "; // pareil que test5String = test5String + myString;
+    }
+    test5String = test5String.Substring(0, test5String.Length - ", ".Length);
+    // Jacques, Jacques, James
+    Console.WriteLine(test5String); // affichera 'Jacques, Jacques, James'
 }
 
 void PrintForI()
@@ -141,5 +160,88 @@ void PrintForI()
         // // 'y != 3', on serait automatiquement passé à 4, ce qui nous aurait mené à une boucle infinie. Avec y < 3, même en cas
         // // de double incrémentation, nous sortirons bien de la boucle.
         // [...]
+    }
+    
+    var integers = new List<int>()
+    {
+        1,
+        45,
+        200,
+        200,
+        150,
+        12
+    };
+
+    for (int i = 0; i < integers.Count; i++)
+    {
+        // "array are zero-based"
+        Console.WriteLine(i); // affichera 1, 45, 200, 200, 150, 12
+    }
+    
+    for (int i = integers.Count - 1; i >= 0; i--)
+    {
+        // "array are zero-based"
+        Console.WriteLine(i); // affichera 12, 150, 200, 200, 45, 1
+    }
+}
+
+void PrintForEach()
+{
+    var integers = new List<int>()
+    {
+        1,
+        45,
+        200,
+        200,
+        150,
+        12
+    };
+
+    int i = 0;
+    foreach (var integer in integers.OrderBy(x => x))
+    {
+        Console.WriteLine($"Index: {i} - Valeur: {integer}"); // affichera 1, 12, 45, 150, 200, 200
+        i++;
+    }
+    
+    foreach (var integer in integers.OrderByDescending(x => x))
+    {
+        Console.WriteLine(integer); // affichera 200, 200, 150, 45, 12, 1 
+    }
+}
+
+void PrintWhile()
+{
+    var integers = new List<int>()
+    {
+        1,
+        45,
+        200,
+        200,
+        150,
+        12
+    };
+
+    int i = 0;
+    // while (i != integers.Count) // risque de boucle infinie
+    while (i <= integers.Count)
+    {
+        Console.WriteLine(integers[i]); 
+        // affichera 1 à la 1ère itération (index 0)
+        // affichera 45 à la 2ème itération (index 1)
+        // affichera 200 à la 3ème itération (index 2)
+        // affichera 200 à la 4ème itération (index 3)
+        // affichera 150 à la 5ème itération (index 4)
+        // affichera 12 à la 6ème itération (index 5)
+        i++;
+    }
+
+    i = 0;
+    while (true)
+    {
+        Console.WriteLine(integers[i]);
+        if (i >= integers.Count)
+            break;
+        // boucle infinie
     }
 }
